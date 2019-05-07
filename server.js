bab8183e02d212f973d4f5e84a9b451cf7b07c4a
Mayank Gupta (IBAM) <Mayank.Gupta@iontrading.com>
2019-05-07 10:46:40 +0530

    Added Project Data

--- server.js
+++ server.js
@@ -3,23 +3,65 @@ var express = require("express");
 var app = express();
 var fetch = require('node-fetch');
 
-app.get("/", function(request, response) {
+var employeeList = [];
+var projectList = [];
+
+var baseUrl = "http://localhost:4000/";
+
+app.get("/getemployeelist", function(request, response) {
 
     fs.readFile("./employeeDetails.json", function(err, data) {
-        var employeeList = JSON.parse(data);
+        data = JSON.parse(data)
+        employeeList = data.employeeListDetails;
         response.send(employeeList);
     })
 });
 
+app.get("/getprojectlist", function(request, response) {
 
-app.get("/user/:username", function(request, response) {
-    fetch("http://localhost:3000/").then(response => response.json()).then(data => {
-        var filteredUsers = data.employeeDetails.filter((data) => {
-            return data.name.toLowerCase() == request.params.username.toLowerCase() ? true: false;
+    fs.readFile("./projectDetails.json", function(err, data) {
+        data = JSON.parse(data)
+        projectList = data.projectListDetails;
+        response.send(projectList);
+    })
+});
+
+
+app.get("/getuserdetails/:userid", function(request, response) {
+    var filteredUsers = null;
+    var filteredProject = null;
+    new Promise((resolve) => {
+        fetch(baseUrl + "getemployeelist").then(response => response.json()).then(data => {
+
+            filteredUsers = data.filter((data) => {
+                return data.employeeId == request.params.userid ? true: false;
+            });
+
+
+            fetch(baseUrl + "getprojectlist").then(response => response.json()).then(projectData => {
+
+                filteredProject = projectData.filter((data) => {
+                    return data.id == filteredUsers[0].projectId ? true: false;
+                });
+
+                var selectedUser = filteredUsers[0];
+
+                console.log(selectedUser);
+
+                console.log(filteredProject[0])
+
+                selectedUser["projectName"] = filteredProject[0].projectName;
+                selectedUser["projectLocation"] = filteredProject[0].projectLocation;
+                selectedUser["projectDuration"] = filteredProject[0].projectDuration;
+
+                resolve(selectedUser);
+    
+            })
+
         })
-
-        response.send(filteredUsers);
+    }).then((data) => {
+        response.send(data);
     })
 })
 
-app.listen(3000);
+app.listen(4000);
